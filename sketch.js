32b4a663b315a82221f878874ea7c440f238ff42
marianabll <100840719+marianabll@users.noreply.github.com>
2022-11-17 15:56:27 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+var caixa;
+
+function setup() {
+  createCanvas(400,400);
+
+  caixa = createSprite(250,180,40,50)
+  caixa.shapeColor = "red"
+}
+
+
+
+function draw() 
+{
+  background(30);
+  drawSprites()
+}
+
+
+
+
